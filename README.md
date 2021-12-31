b318800665f28f9960fd306e89379c3cf3021ba6
guptasai007 <guptasai007@gmail.com>
2022-01-01 03:44:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# user-web-app
+ Spring Boot web-application for CRUD operations around Users.
---
+++ src/test/java/com/abhishek/userwebapp/UserWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.userwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
